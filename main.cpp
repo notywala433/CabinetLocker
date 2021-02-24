144d948ef5cdd7b796bf3b7daf213832883e6795
Thami <notywala@yahoo.com>
2021-02-24 22:18:52 +0200

    test changes 2

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 
 // uncomment this line to add the Live Client Module and use live reloading with your custom C++ code
 //#include <FelgoLiveClient>
-//this is code from the git example
+//this is code from the git example 2
 
 int main(int argc, char *argv[])
 {
